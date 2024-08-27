0e9c5f748b26b199476c120e7b84ae5d5ed6ec63
Andres Ramirez <andres91_07@hotmail.com>
2024-08-27 08:58:03 -0500

    Segundo tutorial: dialogos, confirmaciones, combobox

--- src/mi_aplicacion/__main__.py
+++ src/mi_aplicacion/__main__.py
@@ -3,6 +3,7 @@ from logica.organizador_materias import Organizador_Materias
 from gui.visor_materias import Aplicacion_Gui
 from PyQt5.QtWidgets import QApplication
 
+
 class App(QApplication):
     def __init__(self, sys_argv):
         super(App, self).__init__(sys_argv)
--- src/mi_aplicacion/gui/visor_materias.py
+++ src/mi_aplicacion/gui/visor_materias.py
@@ -1,36 +1,39 @@
 from PyQt5.QtWidgets import QWidget, QPushButton, QHBoxLayout, QGroupBox, QGridLayout, QLabel, QLineEdit, QVBoxLayout
 
+from mi_aplicacion.gui.dialogo_nueva_materia import Dialogo_nueva_materia
+
+
 class Aplicacion_Gui(QWidget):
 
     def __init__(self, logica):
         super().__init__()
 
-        #Se establecen las características de la ventana
+        # Se establecen las características de la ventana
         self.title = 'Mi aplicación'
         self.left = 80
         self.top = 80
         self.width = 300
         self.height = 320
-        #Inicializamos la ventana principal
+        # Inicializamos la ventana principal
         self.inicializar_GUI()
-        #Asignamos el valor de la lógica
+        # Asignamos el valor de la lógica
         self.logica = logica
         self.actualizar_materia()
-    
+
     def inicializar_GUI(self):
-        #inicializamos la ventana
+        # inicializamos la ventana
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
-        #Creamos el distribuidor gráfico principal
+        # Creamos el distribuidor gráfico principal
         self.distr_vertical = QVBoxLayout()
 
-        #Creamos la caja de materias
+        # Creamos la caja de materias
         self.caja_materias = QGroupBox("Materia")
         distr_caja_materias = QGridLayout()
         self.caja_materias.setLayout(distr_caja_materias)
 
-        #Creamos las etiquetas y campos de texto de la materia la caja de materias
+        # Creamos las etiquetas y campos de texto de la materia la caja de materias
         self.etiqueta_nombre = QLabel('Nombre')
         self.txt_nombre = QLineEdit()
         self.txt_nombre.setReadOnly(True)
@@ -47,42 +50,45 @@ class Aplicacion_Gui(QWidget):
         self.txt_nota = QLineEdit()
         self.txt_nota.setReadOnly(True)
 
-        #Agregamos a la caja de materias las etiquetas
-        distr_caja_materias.addWidget(self.etiqueta_nombre, 0,0)
-        distr_caja_materias.addWidget(self.etiqueta_semestre, 1,0)
-        distr_caja_materias.addWidget(self.etiqueta_profesor, 2,0)
-        distr_caja_materias.addWidget(self.etiqueta_nota, 3,0)
+        # Agregamos a la caja de materias las etiquetas
+        distr_caja_materias.addWidget(self.etiqueta_nombre, 0, 0)
+        distr_caja_materias.addWidget(self.etiqueta_semestre, 1, 0)
+        distr_caja_materias.addWidget(self.etiqueta_profesor, 2, 0)
+        distr_caja_materias.addWidget(self.etiqueta_nota, 3, 0)
 
-        #Agregamos a la caja de materias los campos de texto
-        distr_caja_materias.addWidget(self.txt_nombre, 0,1)
-        distr_caja_materias.addWidget(self.txt_semestre, 1,1)
-        distr_caja_materias.addWidget(self.txt_profesor, 2,1)
-        distr_caja_materias.addWidget(self.txt_nota, 3,1)
+        # Agregamos a la caja de materias los campos de texto
+        distr_caja_materias.addWidget(self.txt_nombre, 0, 1)
+        distr_caja_materias.addWidget(self.txt_semestre, 1, 1)
+        distr_caja_materias.addWidget(self.txt_profesor, 2, 1)
+        distr_caja_materias.addWidget(self.txt_nota, 3, 1)
 
-        #Creamos la caja de botones
+        # Creamos la caja de botones
         self.caja_botones = QGroupBox()
         self.caja_botones.setFixedHeight(50)
         distr_caja_botones = QHBoxLayout()
         self.caja_botones.setLayout(distr_caja_botones)
-        
-        #Creamos los botones para la caja de botones
+
+        # Creamos los botones para la caja de botones
         self.boton_retroceder = QPushButton("<<")
         self.boton_retroceder.clicked.connect(self.retroceder_materia)
         self.boton_avanzar = QPushButton(">>")
         self.boton_avanzar.clicked.connect(self.avanzar_materia)
+        self.boton_nueva_materia = QPushButton("Nueva Materia")
+        self.boton_nueva_materia.clicked.connect(self.dialogo_nueva_materia)
 
-        #Agregamos los botones a la caja de botones
+        # Agregamos los botones a la caja de botones
         distr_caja_botones.addWidget(self.boton_retroceder)
         distr_caja_botones.addWidget(self.boton_avanzar)
+        distr_caja_botones.addWidget(self.boton_nueva_materia)
 
-        #Agregamos las cajas a nuestra aplicación
+        # Agregamos las cajas a nuestra aplicación
         self.distr_vertical.addWidget(self.caja_materias)
         self.distr_vertical.addWidget(self.caja_botones)
 
-        #Definimos el distribuidor principal de la ventana.
+        # Definimos el distribuidor principal de la ventana.
         self.setLayout(self.distr_vertical)
 
-        #Hacemos la ventana visible
+        # Hacemos la ventana visible
         self.show()
 
     def actualizar_materia(self):
@@ -95,7 +101,12 @@ class Aplicacion_Gui(QWidget):
     def avanzar_materia(self):
         self.logica.avanzar()
         self.actualizar_materia()
-    
+
     def retroceder_materia(self):
         self.logica.retroceder()
         self.actualizar_materia()
+
+    def dialogo_nueva_materia(self):
+        dialogo = Dialogo_nueva_materia()
+        if dialogo.exec_():
+            self.logica.aniadir_materia(dialogo.dar_valores())
